3edec9b6e721f53923b6ad176b69a09b9259b894
Markus <markustonsaker@gmail.com>
2014-05-23 17:44:33 -0700

    Added code to Unit.java

--- TutorialsDemosAndExtras/src/tutorials/isometricMapTutorial/Unit.java
+++ TutorialsDemosAndExtras/src/tutorials/isometricMapTutorial/Unit.java
@@ -1,5 +1,69 @@
 package tutorials.isometricMapTutorial;
 
+import java.awt.event.KeyEvent;
+
+import game.assets.GraphicalAsset;
+import game.engine.GameInputEventManager;
+
 public class Unit {
 
+	public String type;
+	public int mapX, mapY;
+	public double width = 0.0, height = 0.0;
+	
+	public GraphicalAsset image;
+	private IsometricTileMap tileMap;
+	
+	public Unit(String type, int mapX, int mapY, IsometricTileMap tileMap){
+		this.tileMap = tileMap;
+		
+		this.mapX = mapX;
+		this.mapY = mapY;
+		
+		setType(type);
+	}
+	
+	public void setType(String type){
+		if(type.equals("Archer")){
+			image = tileMap.gameLayer.assetManager.retrieveGraphicalAsset("Archer");
+		}
+		
+		width = image.width;
+		height = image.height;
+	}
+	
+	public void update(){
+		GameInputEventManager inputEvent = tileMap.gameLayer.inputEvent;
+		
+		int newMapX = mapX, newMapY = mapY;
+		
+		if(inputEvent.keyTyped(KeyEvent.VK_UP)){
+			if(tileMap.tiles[mapX+(mapY%2==1?1:0)][mapY-1].isPassable){
+				newMapX += (mapY%2==1?1:0);
+				newMapY--;
+			}
+		}else if(inputEvent.keyTyped(KeyEvent.VK_DOWN)){
+			if(tileMap.tiles[mapX-(mapY%2==0?1:0)][mapY+1].isPassable){
+				newMapX -= (mapY%2==0?1:0);
+				newMapY++;
+			}
+		}
+		if(inputEvent.keyTyped(KeyEvent.VK_LEFT)){
+			if(tileMap.tiles[mapX-(mapY%2==0?1:0)][mapY-1].isPassable){
+				newMapX += (mapY%2==0?1:0);
+				newMapY--;
+			}
+		}else if(inputEvent.keyTyped(KeyEvent.VK_RIGHT)){
+			if(tileMap.tiles[mapX+(mapY%2==1?1:0)][mapY+1].isPassable){
+				newMapX -= (mapY%2==1?1:0);
+				newMapY++;
+			}
+		}
+		
+		if(newMapX != mapX || newMapY != mapY){
+			tileMap.moveUnit(this, mapX, mapY, newMapX, newMapY);
+			mapX = newMapX;
+			mapY = newMapY;
+		}
+	}
 }
