390a032ca7cf792562354395332b3a56ec8f179b
Markusmoo <markustonsaker@gmail.com>
2014-05-26 14:24:29 -0700

    Michael is bad

---
+++ TutorialsDemosAndExtras/src/tutorials/topDownScroller/TopDownScrollerlayer.java
@@ -0,0 +1,12 @@
+package tutorials.topDownScroller;
+
+import game.physics.CollisionSpace;
+
+public class TopDownScrollerlayer extends CollisionSpace {
+	
+	private static final double PARALLAX_VALUE_ISLANDS = 0.4;
+	private static final double PARALLAX_VALUE_CLOUDS = 0.8;
+	private static final double PARALLAX_VALUE_GAMEOBJECTS = 0.8;
+	
+	
+}
